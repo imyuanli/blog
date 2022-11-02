b06059ad692547a676480e801479718821fa3fb3
yuanli <2865437316@qq.com>
2022-11-02 18:22:03 +0800

    no test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
 import './App.css'
-import 'moment/locale/zh-cn';
-// import 'antd/dist/antd.css';
 import 'antd/dist/antd.variable.min.css';
+// 引入custom-default.css 以及 custom-dark.css
+import "./theme/custom-default.css";
+import "./theme/custom-dark.css";
 import './index.css';
 import routes from "./router";
 import {NavLink, useRoutes} from "react-router-dom";
-import {FileOutlined, HomeOutlined} from "@ant-design/icons";
-import {Button, ConfigProvider} from "antd";
+import {
+    AndroidOutlined,
+    AppleOutlined,
+    CheckOutlined,
+    CloseOutlined,
+    FileOutlined,
+    HomeOutlined, IeOutlined,
+    SearchOutlined
+} from "@ant-design/icons";
+import {Button, Calendar, Card, ConfigProvider, Layout, Rate, Switch} from "antd";
+import {useLocalStorageState} from "ahooks";
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+
+const {Header, Footer, Sider, Content} = Layout;
+
 function App() {
+    //router
     const views = useRoutes(routes)
-    const navLinks = [
+
+    //切换主题
+    const [prefix, setPrefix] = useLocalStorageState<string | undefined>(
+        'prefix',
+        {
+            defaultValue: 'custom-default',
+        },
+    );
+    const handlePrefixChange = (prefix: boolean) => {
+        if (prefix) {
+            setPrefix('custom-default')
+        } else {
+            setPrefix('custom-dark')
+        }
+    };
+
+    //active路由
+    const routerArr = [
         {
             path: '/',
             name: '首页',
@@ -25,46 +58,43 @@ function App() {
     const computeClassName = ({isActive}: { isActive: any }) => {
         return isActive ? activeClassName : ""
     }
-
-
-    ConfigProvider.config({
-        theme: {
-            primaryColor: '#25b864',
-        },
-    });
     return (
-        <ConfigProvider prefixCls="custom">
-            <div className="App">
-                <div className={'flex justify-center items-center w-full flex-col'}>
-                    <div className='content mt-6 flex flex-col'>
-                        <Button type={'primary'}>dasd</Button>
-                        <div className={'text-3xl text-gray-500'}>
-                            XXXX
-                        </div>
-                        <div className={'text-sm text-gray-400'}>
-                            ASDASD大苏打
-                        </div>
+        <ConfigProvider prefixCls={prefix}>
+            <div className={`App ${prefix}`}>
+                <div className={'header'}>
+                    <div>
+                        Miracle
                     </div>
-                    <div className='content mt-3 flex items-end flex-col'>
-                        {
-                            navLinks.map((item, index) => {
-                                return (
-                                    <div key={index}>
+                    <div className={'flex justify-center items-center'}>
+                        <Button className={'mr-3 rounded-lg'} icon={<SearchOutlined/>}/>
+                        <>
+                            {
+                                routerArr.map((item, index) => {
+                                    return (
                                         <NavLink
                                             to={item?.path}
-                                            end
-                                            className={computeClassName}
+                                            className={'text-base mr-3'}
+                                            key={index}
                                         >
-                                            {item?.name}
-                                            {item?.icon}
+                                            <Button className={'rounded-lg'} icon={item?.icon}/>
                                         </NavLink>
-                                    </div>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                        </>
+                        <Button className={'rounded-lg'} icon={<DarkModeOutlinedIcon style={{fontSize: 22}}/>}/>
+                        <Switch
+                            checkedChildren={<WbSunnyOutlinedIcon style={{fontSize: 20}}/>}
+                            unCheckedChildren={<DarkModeOutlinedIcon style={{fontSize: 22}}/>}
+                            defaultChecked
+                        />
                     </div>
                 </div>
-                {views}
+                <div style={{paddingTop: 54}} className={'w-full flex justify-center items-center'}>
+                    <div style={{maxWidth: 1000}} className={'w-full'}>
+                        {views}
+                    </div>
+                </div>
             </div>
         </ConfigProvider>
     )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,18 @@
 function Index() {
     return (
-        <div className=''>
-            首页
-        </div>
+        <>
+            <div className={'w-full flex flex-col justify-center items-center mt-10 mb-10'}>
+                <span className={'text-4xl mb-3'}>Miracle</span>
+                <span className={'text-lg'}>Hi, nice to meet you!</span>
+            </div>
+            <div className={'w-full flex flex-col justify-center items-center mt-10 mb-10 b'}>
+                <div className={'bg-white w-2/3 h-48 p-6 rounded-xl shadow-lg'}>
+                    <div className={'text-4xl font-semibold mb-9'}>Card 组件</div>
+                    <div className={'text-base mb-6'} >折叠预览折叠预览折叠预览</div>
+                    <div>2021-01-21</div>
+                </div>
+            </div>
+        </>
     )
 }
 
