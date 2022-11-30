6136073b8ab508099036dcc8278af1265e8985b7
imyuanli <2865437316@qq.com>
2022-11-30 18:21:49 +0800

    获取文章列表 分类列表

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,17 +1,33 @@
 import ProFile from "../components/profile";
 import {useTitle} from "ahooks";
 import ArticleCard from "../components/article-card";
+import {useEffect, useState} from "react";
+import {get_articles_list, get_classify_list} from "../service/service";
 
 const Dashboard = () => {
     useTitle("YuanLi‘s Blog");
+    //获取文章列表
+    const [articleList, setArticleList] = useState([])
+    //分类列表
+    const [classifyList, setClassifyList] = useState([])
+    useEffect(() => {
+        get_articles_list().then((res) => {
+            setArticleList(res)
+        })
+        get_classify_list().then((res) => {
+            setClassifyList(res)
+        })
+    }, [])
     return (
         <div className={'w-full flex flex-col justify-center items-center'}>
             <ProFile userName={"YuanLi"}/>
-            <ArticleCard/>
-            <ArticleCard/>
-            <ArticleCard/>
-            <ArticleCard/>
-            <ArticleCard/>
+            {
+                articleList.map((article, index) => {
+                    return (
+                        <ArticleCard article={article} classifyList={classifyList} key={index}/>
+                    )
+                })
+            }
             <div className={'mb-2 cursor-pointer'}>
                 加载更多
             </div>
