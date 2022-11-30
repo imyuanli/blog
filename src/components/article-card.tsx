72767b83fafdfdf0540bb92cafa399af2069330a
imyuanli <2865437316@qq.com>
2022-11-30 18:22:03 +0800

    文章列表数据渲染

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -11,33 +11,49 @@ import {
 } from "@ant-design/icons";
 import {goToCSDN, goToGitHUb, goToMail, IconFont} from '../utils/util'
 import {NavLink} from "react-router-dom";
+import * as dayjs from 'dayjs'
 
 interface props {
-
+    article: any,
+    classifyList: any
 }
 
-const ArticleCard: React.FunctionComponent<props> = () => {
+const ArticleCard: React.FunctionComponent<props> = ({article, classifyList}) => {
+    const {
+        article_id,
+        title,
+        describe,
+        content,
+        cover,
+        create_time,
+        update_time,
+        keywords,
+        type,
+        classify_name,
+        view_count,
+        comment_count
+    } = article
+
+
+    const getClassifyTitle = (name: string) => {
+        return classifyList.find((item: any) => {
+            return item.classify_name == name
+        })
+    }
     return (
         <Card className={'w-full p-2 rounded-xl shadow-md mb-6 hover:shadow-lg duration-300'}>
             <NavLink
-                to={'article/12'}
+                to={`article/${article_id}`}
                 className={'contents'}
                 style={{color: '#37475b'}}
             >
                 <div>
                     <div className={'text-2xl font-semibold mb-6 truncate overflow-hidden'}>
-                        Card 组件Card 组件Card
-                        组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card
-                        组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件Card 组件
+                        {title}
                     </div>
                     <div className={'flex mb-3'}>
-                        <img src={imgUrl} className={'w-20 h-20 md:w-24 md:h-24 object-cover mr-3'} alt=""/>
-                        <p className={'text-base over-line'}>
-                            描述描述描述描述描述描述描述描述描述描述描述描述
-                            描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述
-                            描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述
-                            描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述描述
-                        </p>
+                        {cover && <img src={cover} className={'w-20 h-20 md:w-24 md:h-24 object-cover mr-3'} alt=""/>}
+                        <p className={'text-base over-line'}>{describe}</p>
                     </div>
                 </div>
             </NavLink>
@@ -45,7 +61,7 @@ const ArticleCard: React.FunctionComponent<props> = () => {
                 <div className={'flex'}>
                     <div className={'mr-2'}>
                         <FieldTimeOutlined className={'mr-1'}/>
-                        <span>2021-01-21</span>
+                        <span>{dayjs(create_time).format('YYYY-MM-DD')}</span>
                     </div>
                     <NavLink
                         to={'/categories'}
@@ -53,16 +69,16 @@ const ArticleCard: React.FunctionComponent<props> = () => {
                     >
                         <div className={'mr-2'}>
                             <FolderOutlined className={'mr-1'}/>
-                            <span>技术博客</span>
+                            <span>{getClassifyTitle(classify_name)?.classify_title}</span>
                         </div>
                     </NavLink>
                     <div className={'mr-1'}>
                         <EyeOutlined className={'mr-1'}/>
-                        <span>123</span>
+                        <span>{view_count}</span>
                     </div>
                     <div>
                         <CommentOutlined className={'m-1'}/>
-                        <span>123</span>
+                        <span>{comment_count}</span>
                     </div>
                 </div>
             </div>
