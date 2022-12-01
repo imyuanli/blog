2cacf3f46dcebb5a413b716232c4a0bc93daabe7
imyuanli <2865437316@qq.com>
2022-12-01 17:49:44 +0800

    请求分类文章

--- src/service/service.js
+++ src/service/service.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://127.0.0.1:8000/api'
 // const BASE_URL = 'http://8.136.80.201:8000/api'
 
 export async function get_articles_list(payload) {
-    return request.get(BASE_URL + '/get_articles_list/', payload)
+    return request.post(BASE_URL + '/get_articles_list/', payload)
 }
 
 export async function get_article(payload) {
